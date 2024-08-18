13b4aa16d401e045e8f63dccfeeba0abf415c878
simetrio <simetrio@yandex.com>
2024-08-18 18:05:45 +0500

    Add connect wallet and choose layer

--- src/src/Navigation.tsx
+++ src/src/Navigation.tsx
@@ -2,28 +2,31 @@ import React from "react"
 import logo from "./logo.svg"
 import {
     Box,
-    Button,
-    IconButton,
     List,
     ListDivider,
     ListItem,
     ListItemButton,
     useColorScheme
 } from "@mui/joy"
-import { DarkMode, Home, LightMode, Person } from "@mui/icons-material"
+import { DarkMode, Home, LightMode, CurrencyBitcoin } from "@mui/icons-material"
 
 export const Navigation = () => {
     const { mode, setMode } = useColorScheme()
 
     return (
-        <Box component="nav" sx={{ flexGrow: 1 }}>
+        <Box
+            sx={{
+                display: "flex",
+                flexGrow: 1,
+                justifyContent: "space-between",
+            }}
+        >
             <List role="menubar" orientation="horizontal" size="lg">
                 <ListItem role="none">
                     <ListItemButton
                         role="menuitem"
                         component="a"
                         href="/"
-                        aria-label="Home"
                     >
                         <Home />
                     </ListItemButton>
@@ -39,18 +42,46 @@ export const Navigation = () => {
                         Liquidity
                     </ListItemButton>
                 </ListItem>
-                <ListItem role="none" sx={{ marginInlineStart: "auto" }}>
-                    <ListItemButton
-                        role="menuitem"
-                        component="button"
-                        onClick={() => {
-                            setMode(mode === "light" ? "dark" : "light");
-                        }}
-                    >
-                        {mode === "light" ? <LightMode /> : <DarkMode />}
-                    </ListItemButton>
-                </ListItem>
             </List>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 1.5,
+                    alignItems: "center",
+                }}
+            >
+                <List role="menubar" orientation="horizontal" size="lg">
+                    <ListItem role="none">
+                        <ListItemButton
+                            role="menuitem"
+                            component="button"
+                            title={mode === "light" ? "Turn dark" : "Turn light"}
+                            onClick={() => {
+                                setMode(mode === "light" ? "dark" : "light");
+                            }}
+                        >
+                            {mode === "light" ? <LightMode /> : <DarkMode />}
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem role="none">
+                        <ListItemButton
+                            role="menuitem"
+                            component="button"
+                        >
+                            <CurrencyBitcoin />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem role="none">
+                        <ListItemButton
+                            color="success"
+                            variant="soft"
+                        >
+                            Connect wallet
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Box>
         </Box>
     )
 }
