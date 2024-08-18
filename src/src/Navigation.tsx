f6d63926fa6b7f744e393b637d8e1089dd28411b
simetrio <simetrio@yandex.com>
2024-08-18 13:28:43 +0500

    Top menu

--- src/src/Navigation.tsx
+++ src/src/Navigation.tsx
@@ -1,26 +1,56 @@
 import React from "react"
 import logo from "./logo.svg"
 import {
+    Box,
     Button,
     IconButton,
+    List,
+    ListDivider,
+    ListItem,
+    ListItemButton,
     useColorScheme
 } from "@mui/joy"
-import { DarkMode, LightMode } from "@mui/icons-material"
+import { DarkMode, Home, LightMode, Person } from "@mui/icons-material"
 
 export const Navigation = () => {
     const { mode, setMode } = useColorScheme()
 
     return (
-        <>
-            <IconButton
-                component="button"
-                size="lg"
-                onClick={() => {
-                    setMode(mode === "light" ? "dark" : "light");
-                }}
-            >
-                {mode === "light" ? <LightMode /> : <DarkMode />}
-            </IconButton>
-        </>
+        <Box component="nav" sx={{ flexGrow: 1 }}>
+            <List role="menubar" orientation="horizontal" size="lg">
+                <ListItem role="none">
+                    <ListItemButton
+                        role="menuitem"
+                        component="a"
+                        href="/"
+                        aria-label="Home"
+                    >
+                        <Home />
+                    </ListItemButton>
+                </ListItem>
+                <ListDivider />
+                <ListItem role="none">
+                    <ListItemButton role="menuitem" component="a" href="/swap">
+                        Swap
+                    </ListItemButton>
+                </ListItem>
+                <ListItem role="none">
+                    <ListItemButton role="menuitem" component="a" href="/liquidity">
+                        Liquidity
+                    </ListItemButton>
+                </ListItem>
+                <ListItem role="none" sx={{ marginInlineStart: "auto" }}>
+                    <ListItemButton
+                        role="menuitem"
+                        component="button"
+                        onClick={() => {
+                            setMode(mode === "light" ? "dark" : "light");
+                        }}
+                    >
+                        {mode === "light" ? <LightMode /> : <DarkMode />}
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Box>
     )
 }
