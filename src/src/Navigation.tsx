5515b6d072c5b2c77b5b88fa3cc2ea2a999863d2
simetrio <simetrio@yandex.com>
2024-08-18 18:35:44 +0500

    Header styles

--- src/src/Navigation.tsx
+++ src/src/Navigation.tsx
@@ -7,120 +7,152 @@ import {
     ListDivider,
     ListItem,
     ListItemButton,
+    ListItemDecorator,
     Menu,
     MenuButton,
     MenuItem,
     useColorScheme
 } from "@mui/joy"
-import { DarkMode, Home, LightMode, CurrencyBitcoin } from "@mui/icons-material"
+import { DarkMode, Home, LightMode, CurrencyBitcoin, Check } from "@mui/icons-material"
 
 export const Navigation = () => {
     const { mode, setMode } = useColorScheme()
 
     return (
         <Box
-            sx={{
-                display: "flex",
-                flexGrow: 1,
-                justifyContent: "space-between",
-            }}
+            component="header"
+            className="Header"
+            sx={[
+                {
+                    p: 1,
+                    gap: 2,
+                    bgcolor: "background.surface",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    gridColumn: "1 / -1",
+                    borderBottom: "1px solid",
+                    borderColor: "divider",
+                    position: "sticky",
+                    top: 0,
+                    zIndex: 1100,
+                },
+            ]}
         >
-            <List role="menubar" orientation="horizontal" size="lg">
-                <ListItem role="none">
-                    <ListItemButton
-                        role="menuitem"
-                        component="a"
-                        href="/"
-                    >
-                        <Home />
-                    </ListItemButton>
-                </ListItem>
-                <ListDivider />
-                <ListItem role="none">
-                    <ListItemButton role="menuitem" component="a" href="/swap">
-                        Swap
-                    </ListItemButton>
-                </ListItem>
-                <ListItem role="none">
-                    <ListItemButton role="menuitem" component="a" href="/liquidity">
-                        Liquidity
-                    </ListItemButton>
-                </ListItem>
-            </List>
             <Box
                 sx={{
                     display: "flex",
-                    flexDirection: "row",
-                    gap: 1.5,
-                    alignItems: "center",
+                    flexGrow: 1,
+                    justifyContent: "space-between",
                 }}
             >
                 <List role="menubar" orientation="horizontal" size="lg">
                     <ListItem role="none">
                         <ListItemButton
                             role="menuitem"
-                            component="button"
-                            title={mode === "light" ? "Turn dark" : "Turn light"}
-                            onClick={() => {
-                                setMode(mode === "light" ? "dark" : "light");
-                            }}
+                            component="a"
+                            href="/"
                         >
-                            {mode === "light" ? <LightMode /> : <DarkMode />}
+                            <Home />
                         </ListItemButton>
                     </ListItem>
-                    <Dropdown>
-                        <MenuButton
-                            variant="plain"
-                            size="lg"
-                            sx={{ maxWidth: "58px", maxHeight: "42px", borderRadius: "0px" }}
-                        >
-                            <CurrencyBitcoin />
-                        </MenuButton>
-                        <Menu
-                            placement="bottom-end"
-                            size="md"
-                            sx={{
-                                zIndex: "99999",
-                                p: 1,
-                                gap: 1,
-                                "--ListItem-radius": "var(--joy-radius-sm)",
-                            }}
-                        >
-                            <MenuItem>
-                                <CurrencyBitcoin />
-                                Ethereum
-                            </MenuItem>
-                            <MenuItem>
-                                <CurrencyBitcoin />
-                                Arbitrum
-                            </MenuItem>
-                            <MenuItem>
-                                <CurrencyBitcoin />
-                                Optimism
-                            </MenuItem>
-                            <MenuItem>
-                                <CurrencyBitcoin />
-                                Poligon
-                            </MenuItem>
-                            <MenuItem>
-                                <CurrencyBitcoin />
-                                BNB Chain
-                            </MenuItem>
-                            <MenuItem>
-                                <CurrencyBitcoin />
-                                Avalanche
-                            </MenuItem>
-                        </Menu>
-                    </Dropdown>
+                    <ListDivider />
                     <ListItem role="none">
-                        <ListItemButton
-                            color="success"
-                            variant="soft"
-                        >
-                            Connect wallet
+                        <ListItemButton role="menuitem" component="a" href="/swap">
+                            Swap
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem role="none">
+                        <ListItemButton role="menuitem" component="a" href="/liquidity">
+                            Liquidity
                         </ListItemButton>
                     </ListItem>
                 </List>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: 1.5,
+                        alignItems: "center",
+                    }}
+                >
+                    <List role="menubar" orientation="horizontal" size="lg">
+                        <ListItem role="none">
+                            <ListItemButton
+                                role="menuitem"
+                                component="button"
+                                title={mode === "light" ? "Turn dark" : "Turn light"}
+                                onClick={() => {
+                                    setMode(mode === "light" ? "dark" : "light");
+                                }}
+                            >
+                                {mode === "light" ? <LightMode /> : <DarkMode />}
+                            </ListItemButton>
+                        </ListItem>
+                        <Dropdown>
+                            <MenuButton
+                                variant="plain"
+                                size="lg"
+                                title="Choose layer"
+                                sx={{ maxWidth: "58px", maxHeight: "42px", borderRadius: "0px" }}
+                            >
+                                <CurrencyBitcoin />
+                            </MenuButton>
+                            <Menu
+                                placement="bottom-end"
+                                size="md"
+                                sx={{
+                                    zIndex: "99999",
+                                    p: 1,
+                                    gap: 1,
+                                    "--ListItem-radius": "var(--joy-radius-sm)",
+                                }}
+                            >
+                                <MenuItem>
+                                    <CurrencyBitcoin />
+                                    Ethereum
+                                    <ListItemDecorator />
+                                </MenuItem>
+                                <MenuItem>
+                                    <CurrencyBitcoin />
+                                    Arbitrum
+                                    <ListItemDecorator>
+                                        <Check />
+                                    </ListItemDecorator>
+                                </MenuItem>
+                                <MenuItem>
+                                    <CurrencyBitcoin />
+                                    Optimism
+                                    <ListItemDecorator />
+                                </MenuItem>
+                                <MenuItem>
+                                    <CurrencyBitcoin />
+                                    Poligon
+                                    <ListItemDecorator />
+                                </MenuItem>
+                                <MenuItem>
+                                    <CurrencyBitcoin />
+                                    BNB Chain
+                                    <ListItemDecorator />
+                                </MenuItem>
+                                <MenuItem>
+                                    <CurrencyBitcoin />
+                                    Avalanche
+                                    <ListItemDecorator />
+                                </MenuItem>
+                            </Menu>
+                        </Dropdown>
+                        <ListItem role="none">
+                            <ListItemButton
+                                color="success"
+                                variant="soft"
+                            >
+                                Connect wallet
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Box>
             </Box>
         </Box>
     )
