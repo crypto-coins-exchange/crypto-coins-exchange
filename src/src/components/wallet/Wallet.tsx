3a27b64912bd0e53436078d8270dcc82c124143f
simetrio <simetrio@yandex.com>
2024-08-20 00:21:45 +0500

    Wallet

--- src/src/components/wallet/Wallet.tsx
+++ src/src/components/wallet/Wallet.tsx
@@ -1,4 +1,6 @@
-import { Box } from "@mui/joy"
+import { Box, Grid, Stack, Typography } from "@mui/joy"
+import { Icons } from "../common/Icons"
+import { Chain } from "../../lib/chains"
 
 export const Wallet = () => {
     return (
@@ -16,7 +18,57 @@ export const Wallet = () => {
                     }
                 }
             >
-                
+                <Stack spacing={4}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <Icons chain={Chain.Ethereum} />
+                        <Typography level="title-lg">
+                            0x98997c9a68b...1d9c3533f9e5d1a1
+                        </Typography>
+                    </Stack>
+
+                    <Typography level="h2" component="h3">
+                        $1000.00
+                    </Typography>
+
+                    <Grid container spacing={2}>
+                        <Grid xs={6}>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <Icons chain={Chain.Ethereum} />
+                                <Typography level="title-lg">
+                                    ETH
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid xs={6} textAlign="right">$100.00</Grid>
+                        <Grid xs={6}>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <Icons chain={Chain.Ethereum} />
+                                <Typography level="title-lg">
+                                    ETH
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid xs={6} textAlign="right">$100.00</Grid>
+                        <Grid xs={6}>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <Icons chain={Chain.Ethereum} />
+                                <Typography level="title-lg">
+                                    ETH
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid xs={6} textAlign="right">$100.00</Grid>
+                        <Grid xs={6}>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <Icons chain={Chain.Ethereum} />
+                                <Typography level="title-lg">
+                                    ETH
+                                </Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid xs={6} textAlign="right">$100.00</Grid>
+                    </Grid>
+                </Stack>
             </Box>
         </>
     )
