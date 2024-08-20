e53772a0f0f4fb3e628197b1e1399cca2531b09e
simetrio <simetrio@yandex.com>
2024-08-20 18:27:13 +0500

    Show disconnected window

--- src/src/components/App.tsx
+++ src/src/components/App.tsx
@@ -47,7 +47,7 @@ function App() {
         }}>
         <Navigation {...stateProps} />
         <Body />
-        <Wallet />
+        <Wallet {...stateProps} />
       </Box>
 
     </CssVarsProvider>
--- src/src/components/wallet/Wallet.tsx
+++ src/src/components/wallet/Wallet.tsx
@@ -1,75 +1,92 @@
-import { Box, Grid, Stack, Typography } from "@mui/joy"
+import { Box, Button, Grid, Stack, Typography } from "@mui/joy"
 import { Icons } from "../common/Icons"
-import { Chain } from "../../lib/chains"
+import { StateProps } from "../../lib/state"
+import { AccountBalanceWallet } from "@mui/icons-material"
 
-export const Wallet = () => {
+interface Props extends StateProps {
+
+}
+
+export const Wallet = (props: Props) => {
     return (
-        <>
-            <Box
-                component="nav"
-                className="Navigation"
-                sx={
-                    {
-                        p: 2,
-                        bgcolor: "background.surface",
-                        borderLeft: "1px solid",
-                        borderColor: "divider",
-                        height: "100%",
-                    }
+        <Box
+            component="nav"
+            className="Navigation"
+            sx={
+                {
+                    p: 2,
+                    bgcolor: "background.surface",
+                    borderLeft: "1px solid",
+                    borderColor: "divider",
+                    height: "100%",
                 }
-            >
-                <Stack spacing={4}>
+            }
+        >
+            {!!props.state.wallet.address ? <ConnectedWallet {...props} /> : <NotConnectedWallet />}
+        </Box>
+    )
+}
+
+export const ConnectedWallet = (props: Props) => {
+    return (
+        <Stack spacing={4}>
+            <Stack direction="row" spacing={1} alignItems="center">
+                <Icons chain={props.state.chain.chain} />
+                <Typography level="title-lg">
+                    0x98997c9a68b...1d9c3533f9e5d1a1
+                </Typography>
+            </Stack>
+
+            <Typography level="h2" component="h3">
+                $1000.00
+            </Typography>
+
+            <Grid container spacing={2}>
+                <Grid xs={6}>
                     <Stack direction="row" spacing={1} alignItems="center">
-                        <Icons chain={Chain.Ethereum} />
+                        <Icons chain={props.state.chain.chain} />
                         <Typography level="title-lg">
-                            0x98997c9a68b...1d9c3533f9e5d1a1
+                            ETH
                         </Typography>
                     </Stack>
+                </Grid>
+                <Grid xs={6} textAlign="right">$100.00</Grid>
+                <Grid xs={6}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <Icons chain={props.state.chain.chain} />
+                        <Typography level="title-lg">
+                            ETH
+                        </Typography>
+                    </Stack>
+                </Grid>
+                <Grid xs={6} textAlign="right">$100.00</Grid>
+                <Grid xs={6}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <Icons chain={props.state.chain.chain} />
+                        <Typography level="title-lg">
+                            ETH
+                        </Typography>
+                    </Stack>
+                </Grid>
+                <Grid xs={6} textAlign="right">$100.00</Grid>
+                <Grid xs={6}>
+                    <Stack direction="row" spacing={1} alignItems="center">
+                        <Icons chain={props.state.chain.chain} />
+                        <Typography level="title-lg">
+                            ETH
+                        </Typography>
+                    </Stack>
+                </Grid>
+                <Grid xs={6} textAlign="right">$100.00</Grid>
+            </Grid>
+        </Stack>
+    )
+}
 
-                    <Typography level="h2" component="h3">
-                        $1000.00
-                    </Typography>
-
-                    <Grid container spacing={2}>
-                        <Grid xs={6}>
-                            <Stack direction="row" spacing={1} alignItems="center">
-                                <Icons chain={Chain.Ethereum} />
-                                <Typography level="title-lg">
-                                    ETH
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid xs={6} textAlign="right">$100.00</Grid>
-                        <Grid xs={6}>
-                            <Stack direction="row" spacing={1} alignItems="center">
-                                <Icons chain={Chain.Ethereum} />
-                                <Typography level="title-lg">
-                                    ETH
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid xs={6} textAlign="right">$100.00</Grid>
-                        <Grid xs={6}>
-                            <Stack direction="row" spacing={1} alignItems="center">
-                                <Icons chain={Chain.Ethereum} />
-                                <Typography level="title-lg">
-                                    ETH
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid xs={6} textAlign="right">$100.00</Grid>
-                        <Grid xs={6}>
-                            <Stack direction="row" spacing={1} alignItems="center">
-                                <Icons chain={Chain.Ethereum} />
-                                <Typography level="title-lg">
-                                    ETH
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                        <Grid xs={6} textAlign="right">$100.00</Grid>
-                    </Grid>
-                </Stack>
-            </Box>
-        </>
+export const NotConnectedWallet = () => {
+    return (
+        <Box alignItems="center" textAlign="center" display="grid" height="100%" px={5}>
+            <Button startDecorator={<AccountBalanceWallet />} variant="soft">Wallet is not connected</Button>
+        </Box>
     )
 }
